b2bde6db25f1aa9fc382be48156fecf7cd901b54
freddie h <frederico02@hotmail.com>
2023-04-09 15:55:17 +0100

    00011 - finish

--- solutions/00011-easy-tuple-to-object.ts
+++ solutions/00011-easy-tuple-to-object.ts
@@ -20,7 +20,9 @@
 
 /* _____________ Your Code Here _____________ */
 
-type TupleToObject<T extends readonly any[]> = any
+type TupleToObject<T extends readonly (string | number)[]> = {
+  [Prop in T[number]]: Prop
+}
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
@@ -30,9 +32,21 @@ const tupleNumber = [1, 2, 3, 4] as const
 const tupleMix = [1, '2', 3, '4'] as const
 
 type cases = [
-  Expect<Equal<TupleToObject<typeof tuple>, { tesla: 'tesla'; 'model 3': 'model 3'; 'model X': 'model X'; 'model Y': 'model Y' }>>,
+  Expect<
+    Equal<
+      TupleToObject<typeof tuple>,
+      {
+        tesla: 'tesla'
+        'model 3': 'model 3'
+        'model X': 'model X'
+        'model Y': 'model Y'
+      }
+    >
+  >,
   Expect<Equal<TupleToObject<typeof tupleNumber>, { 1: 1; 2: 2; 3: 3; 4: 4 }>>,
-  Expect<Equal<TupleToObject<typeof tupleMix>, { 1: 1; '2': '2'; 3: 3; '4': '4' }>>,
+  Expect<
+    Equal<TupleToObject<typeof tupleMix>, { 1: 1; '2': '2'; 3: 3; '4': '4' }>
+  >,
 ]
 
 // @ts-expect-error
