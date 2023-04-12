5723e63d445b2579ec46cbb2fa43ab0515647948
freddie h <frederico02@hotmail.com>
2023-04-12 10:43:21 +0100

    00008 - finish

--- solutions/00008-medium-readonly-2.ts
+++ solutions/00008-medium-readonly-2.ts
@@ -34,20 +34,11 @@
 
 /* _____________ Your Code Here _____________ */
 
-type MyReadonly2<T, K> = any
-
-/* _____________ Test Cases _____________ */
-import type { Alike, Expect } from '@type-challenges/utils'
-
-type cases = [
-  Expect<Alike<MyReadonly2<Todo1>, Readonly<Todo1>>>,
-  Expect<Alike<MyReadonly2<Todo1, 'title' | 'description'>, Expected>>,
-  Expect<Alike<MyReadonly2<Todo2, 'title' | 'description'>, Expected>>,
-  Expect<Alike<MyReadonly2<Todo2, 'description' >, Expected>>,
-]
-
-// @ts-expect-error
-type error = MyReadonly2<Todo1, 'title' | 'invalid'>
+type MyReadonly2<T extends Record<string, any>, K extends keyof T = keyof T> = {
+  readonly [Prop in K]: T[Prop]
+} & {
+  [Prop in keyof T as Prop extends K ? never : Prop]: T[Prop]
+}
 
 interface Todo1 {
   title: string
@@ -66,6 +57,22 @@ interface Expected {
   readonly description?: string
   completed: boolean
 }
+
+type a = MyReadonly2<Todo1, 'title' | 'description'>
+type b = MyReadonly2<Todo1>
+
+// @ts-expect-error
+type error = MyReadonly2<Todo1, 'title' | 'invalid'>
+
+/* _____________ Test Cases _____________ */
+import type { Alike, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Alike<MyReadonly2<Todo1>, Readonly<Todo1>>>,
+  Expect<Alike<MyReadonly2<Todo1, 'title' | 'description'>, Expected>>,
+  Expect<Alike<MyReadonly2<Todo2, 'title' | 'description'>, Expected>>,
+  Expect<Alike<MyReadonly2<Todo2, 'description'>, Expected>>,
+]
 
 /* _____________ Further Steps _____________ */
 /*
