20ff99e8728744402e292b3bd9545fd43c80420f
freddie h <frederico02@hotmail.com>
2023-04-14 14:08:10 +0100

    00016 - finish

--- solutions/00016-medium-pop.ts
+++ solutions/00016-medium-pop.ts
@@ -26,7 +26,13 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Pop<T extends any[]> = any
+type Pop<T extends any[]> = T extends [...infer FirstSet, infer _] ? FirstSet : T
+
+type arr1 = ['a', 'b', 'c', 'd']
+type arr2 = [3, 2, 1]
+
+type re1 = Pop<arr1> // expected to be ['a', 'b', 'c']
+type re2 = Pop<arr2> // expected to be [3, 2]
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
