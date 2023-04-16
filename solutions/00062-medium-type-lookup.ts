40e92d2bcd872e53cb657c579a6e0dba7cab9585
freddie h <frederico02@hotmail.com>
2023-04-16 21:08:26 +0100

    00062 - finish

--- solutions/00062-medium-type-lookup.ts
+++ solutions/00062-medium-type-lookup.ts
@@ -29,7 +29,20 @@
 
 /* _____________ Your Code Here _____________ */
 
-type LookUp<U, T> = any
+type LookUp<U, T> = U extends { 'type': T } ? U : never // this is basic union discrimination
+
+interface Cat {
+  type: 'cat'
+  breeds: 'Abyssinian' | 'Shorthair' | 'Curl' | 'Bengal'
+}
+
+interface Dog {
+  type: 'dog'
+  breeds: 'Hound' | 'Brittany' | 'Bulldog' | 'Boxer'
+  color: 'brown' | 'white' | 'black'
+}
+
+type MyDogType = LookUp<Cat | Dog, 'dog'> // expected to be `Dog`
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
