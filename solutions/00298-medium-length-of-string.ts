cd5f6ddba4cfb0ccbae94034bc9151ee41c16d47
freddie h <frederico02@hotmail.com>
2023-05-18 19:28:55 +0100

    00298 - finish

--- solutions/00298-medium-length-of-string.ts
+++ solutions/00298-medium-length-of-string.ts
@@ -11,8 +11,24 @@
 */
 
 /* _____________ Your Code Here _____________ */
+type StrToArr<S> = S extends `${infer x}${infer xs}` ? [1, ...StrToArr<xs>] : []
+type b = StrToArr<'abc'>
 
-type LengthOfString<S extends string> = any
+type LengthOfString<S extends string> = StrToArr<S>['length']
+type a = LengthOfString<'aa'>
+
+type LenOStr<S extends string, T extends string[] = []> = S extends `${infer A}${infer B}` ? LenOStr<B, [A, ...T]> : T['length']
+
+// iterations
+// first i, A = a, B = bcd, T = [a]
+// second i, A = b, B = cd, T = [a, b]
+// third i, A = c, B = d, T = [a,b,c]
+// fourth i, A = d, B = never, T = [a,b,c,d]
+// fifth i, A = never, B = never, T['length] // 4
+
+// key insight is you can't just get the prop of a string, must be an array first.
+
+type c = LenOStr<'abcd'>
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
