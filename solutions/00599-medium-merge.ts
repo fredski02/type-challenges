cac1107b4fb05c768e710cd9f5d28811d05b15ac
freddie h <frederico02@hotmail.com>
2023-05-23 21:47:02 +0100

    00599

--- solutions/00599-medium-merge.ts
+++ solutions/00599-medium-merge.ts
@@ -27,7 +27,22 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Merge<F, S> = any
+type Merge<F extends {}, S extends {}> = {
+  [Prop in keyof F | keyof S]: Prop extends keyof S ?
+    S[Prop] : Prop extends keyof F ?
+      F[Prop] : never
+}
+
+type foo = {
+  name: string
+  age: string
+}
+type coo = {
+  age: number
+  sex: string
+}
+
+type Result = Merge<foo, coo> // expected to be {name: string, age: number, sex: string}
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
