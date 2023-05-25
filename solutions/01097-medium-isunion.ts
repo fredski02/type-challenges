b50d725a65bf60afbb401197ce0c1c8567b6e1dd
freddie h <frederico02@hotmail.com>
2023-05-25 21:17:09 +0100

    more weird stuff 01097 - finished

--- solutions/01097-medium-isunion.ts
+++ solutions/01097-medium-isunion.ts
@@ -20,7 +20,11 @@
 
 /* _____________ Your Code Here _____________ */
 
-type IsUnion<T> = any
+type IsUnion<T, B = T> = [T] extends [never] ? false : T extends B ? [B] extends [T] ? false : true : never
+
+type case1 = IsUnion<string> // false
+type case2 = IsUnion<string | number> // true
+type case3 = IsUnion<[string | number]> // false
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
