af6601f07d66885a99e56e8aac7b70f4f45fe9c2
Freddie H <frederico02@hotmail.com>
2023-12-21 17:53:21 +0000

    finish 03326

--- solutions/03326-medium-bem-style-string.ts
+++ solutions/03326-medium-bem-style-string.ts
@@ -16,7 +16,9 @@
 
 /* _____________ Your Code Here _____________ */
 
-type BEM<B extends string, E extends string[], M extends string[]> = any
+// type BEM<B extends string, E extends string[], M extends string[]> = `${B}${E extends [] ? '' : '__'}${E extends [] ? '' : E[number]}${M extends [] ? '' : '--'}${M extends [] ? '' : M[number]}`
+
+type BEM<B extends string, E extends string[], M extends string[]> = `${B}${E extends [] ? '' : `__${E[number]}`}${M extends [] ? '' : `--${M[number]}`}`
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
