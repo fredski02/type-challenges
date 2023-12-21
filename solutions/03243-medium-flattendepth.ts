d2dad176e48c0dadfc98e5f41d144f25bffbf5c7
Freddie H <frederico02@hotmail.com>
2023-12-21 17:42:18 +0000

    finish 03243

--- solutions/03243-medium-flattendepth.ts
+++ solutions/03243-medium-flattendepth.ts
@@ -21,10 +21,18 @@
 
 /* _____________ Your Code Here _____________ */
 
-type FlattenDepth = any
+type FlattenDepth<Arr extends Array<unknown>, Depth = 1, Return extends Array<unknown> = [] > = Arr extends [infer First, ...infer Rest]
+  ? First extends unknown[] // Is first element an array
+    ? Return['length'] extends Depth // does the return array have a length that equals the depth
+      ? [First, ...FlattenDepth<Rest, Depth, Return>] // arr that takes the first and then attempts to flatten the rest
+      : [...FlattenDepth<First, Depth, [0, ...Return]>, ...FlattenDepth<Rest, Depth, Return>] // still more work to do on the first element
+    : [First, ...FlattenDepth<Rest, Depth, Return>] // first element is just a scaler so recursive try the next data
+  : Arr
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
+
+type A = FlattenDepth<[1, [[2]]]>
 
 type cases = [
   Expect<Equal<FlattenDepth<[]>, []>>,
